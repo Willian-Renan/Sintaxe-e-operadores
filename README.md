7b6ea2fd2459ebf002179d4f3e648a0ab130a14e
Willian <willian.tec@outlook.com>
2022-01-03 22:29:33 -0300

    upload

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Sintaxe-e-operadores
+# Sintaxe e Operadores
+
+## Atividade Proposta
+
+- Crie uma função que recebe dois números como parâmetros.
+- Confira se os números são iguais.
+- Confira se a soma dos números é maior que 10 ou menor que 20.
+- Retorne uma string dizendo "Os números `num1` e `num2` não/são iguais. Sua soma é `soma`, que é `maior/menor` que 10 e `maior/menor` que 20".
+
+Exemplo:
+
+```
+Input: 1, 2
+Output: Os números 1 e 2 não são iguais. Sua soma é 3, que é menor que 10 e menor que 20.
+```
+
+Foi realizado a atividade proposta em javascript e utlizado o comando 'alert()' para informar a mensagem no navegador
+
+Link: https://github.com/Willian-Renan/Sintaxe-e-operadores
---
+++ comparaNumeros.js
@@ -0,0 +1,51 @@
+function comparaNumeros(num1, num2) {
+	const primeiraFrase = criaPrimeiraFrase(num1, num2);
+	const segundaFrase = criaSegundaFrase(num1, num2);
+
+	return `${primeiraFrase} ${segundaFrase}`;
+}
+
+let num1 = Number(prompt('Insira o primeiro valor:'));
+let num2 = Number(prompt('Insira o segundo valor:'));
+
+if (!num1 || !num2) {
+	alert('Insira um número em cada variável');
+	comparaNumeros();
+} else {
+
+function criaPrimeiraFrase(num1, num2) {
+	let primeiraFrase = `Os números ${num1} e ${num2}`;
+	let simNao = 'não';
+
+	if (num1 === num2) {
+		simNao = '';
+	}
+
+	return alert(`${primeiraFrase} ${simNao} são iguais.`);
+}
+
+function criaSegundaFrase(num1, num2) {
+	const soma = num1 + num2;
+	let comparaDez = 'menor';
+	let comparaVinte = 'menor';
+
+	if (soma > 10) {
+		comparaDez = 'maior que';
+	}
+
+	if (soma === 10) {
+		comparaDez = 'igual a';
+	}
+
+	if (soma > 20) {
+		comparaVinte = 'maior que';
+	}
+
+	if (soma === 20) {
+		comparaVinte = 'igual a';
+	}
+
+	return alert(`Sua soma é ${soma}, que é ${comparaDez} 10 e ${comparaVinte} 20.`);
+}
+}
+console.log(comparaNumeros(num1, num2));
